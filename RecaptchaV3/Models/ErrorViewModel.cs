95b29960896188594efd5f22f402f961ab2bb3ac
Huseyin6 <hsynsimsek723@gmail.com>
2020-01-31 10:13:36 +0300

    Add project files.

---
+++ RecaptchaV3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecaptchaV3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
