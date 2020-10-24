b34241dc05019911c16662428132786ab92616ff
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-10-24 21:37:56 +0300

    Upgrade Asp.Net Core MVC 2.1 - > 3.1

--- RecaptchaV3/Controllers/HomeController.cs
+++ RecaptchaV3/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using RecaptchaV3.Models;
 
@@ -12,12 +13,17 @@ namespace RecaptchaV3.Controllers
 {
     public class HomeController : Controller
     {
+        private IConfiguration _configuration { get; }
+
         private readonly IHttpClientFactory _clientFactory;
-        private static string googleSecretKey = "YOUR-SECRETKEY-HERE";
-        private static string googleRecaptchaVerifyApi = "https://www.google.com/recaptcha/api/siteverify";
-        public HomeController(IHttpClientFactory clientFactory)
+        private static string googleSecretKey { get; set; }
+        private static string googleRecaptchaVerifyApi { get; set; }
+        public HomeController(IConfiguration configuration, IHttpClientFactory clientFactory)
         {
+            _configuration = configuration;
             _clientFactory = clientFactory;
+            googleSecretKey = configuration.GetSection("GoogleRecaptcha").GetSection("Secretkey").Value ?? "";
+            googleRecaptchaVerifyApi = configuration.GetSection("GoogleRecaptcha").GetSection("VefiyAPIAddress").Value ?? "";
         }
         public IActionResult Index()
         {
@@ -56,6 +62,7 @@ namespace RecaptchaV3.Controllers
                 var response = await client.GetStringAsync($"{googleRecaptchaVerifyApi}?secret={googleSecretKey}&response={token}");
                 tokenResponse = JsonConvert.DeserializeObject<TokenResponseModel>(response);
             }
+
             // Recaptcha V3 Verify Api send score 0-1. If score is low such as less than 0.5, you can think that it is a bot and return false.     
             // If token is not success then return false
             if (!tokenResponse.Success || tokenResponse.Score < (decimal)0.5)
